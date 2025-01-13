e82fcff625f36d110bb1938e4a85f244ab9d9cb8
pstrohal <pstrohal@student.42heilbronn.de>
2025-01-13 14:28:52 +0100

    comment added

--- ex03/src/Intern.cpp
+++ ex03/src/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: pstrohal <pstrohal@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/12 13:11:16 by pstrohal          #+#    #+#             */
-/*   Updated: 2025/01/12 15:58:40 by pstrohal         ###   ########.fr       */
+/*   Updated: 2025/01/13 14:21:35 by pstrohal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 AForm *Intern::makeForm(std::string name, std::string target)
 {
 	static const std::string arr[]  = {"ShrubberyCreationForm", "RobotomyRequestForm", "PresidentialPardonForm"};
-	static const std::function<AForm *(std::string)> Form[] = {
+	static const std::function<AForm *(std::string)> Form[] = {  //array of std::function obj (callable objs) that contain lambdas (temporary functions)
 		[=](std::string){return new ShrubberyCreationForm(target);},
 		[=](std::string){return new RobotomyRequestForm(target);},
 		[=](std::string){return new PresidentialPardonForm(target);}
